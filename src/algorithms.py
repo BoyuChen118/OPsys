2c251ae4ae7460389d9b8c5504f71bbf252d152f
deanvogel <dnv185@hotmail.com>
2021-07-24 17:36:31 -0400

    started
    Co-authored-by: alexboyuchen@gmail.com <alexboyuchen@gmail.com>

---
+++ src/algorithms.py
@@ -0,0 +1,12 @@
+# First-come-first-served
+def fcfs():
+	pass
+# Shortest job first
+def sjf():
+	pass
+# Shortest remaining time
+def srt():
+	pass
+# Round robin
+def rr():
+	pass
---
+++ src/project.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+if __name__ == '__main__':
+	_, n, seed, λ, upper_bound, context_switch_time, α, time_slice = sys.argv
+	pass
+
+
